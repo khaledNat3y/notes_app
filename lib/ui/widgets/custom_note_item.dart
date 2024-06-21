42e5b5c9b5175f0c64134c4e26e7aa1d64e3fe2f
Nat3y <khalednatey85@gmail.com>
2024-06-21 21:35:28 +0300

    -create edit note view. -create edit note view body widget.

--- lib/ui/widgets/custom_note_item.dart
+++ lib/ui/widgets/custom_note_item.dart
@@ -1,54 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/ui/widgets/edit_note_view_body.dart';
+
+import '../edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 8),
-      margin: const EdgeInsets.symmetric(vertical: 8),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              "Flutter Tips",
-              style: TextStyle(color: Colors.black, fontSize: 26),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const EditNoteView()));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 8),
+        margin: const EdgeInsets.symmetric(vertical: 8),
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                "Flutter Tips",
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Text(
+                  "Build your career with khaled natey",
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(0.5), fontSize: 18),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.trash,
+                  size: 24,
+                  color: Colors.black,
+                ),
+              ),
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16),
+            Padding(
+              padding: const EdgeInsets.only(
+                right: 24,
+              ),
               child: Text(
-                "Build your career with khaled natey",
+                "May21, 2022",
                 style: TextStyle(
-                    color: Colors.black.withOpacity(0.5), fontSize: 18),
+                    color: Colors.black.withOpacity(0.4), fontSize: 16),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                FontAwesomeIcons.trash,
-                size: 24,
-                color: Colors.black,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-              right: 24,
-            ),
-            child: Text(
-              "May21, 2022",
-              style:
-                  TextStyle(color: Colors.black.withOpacity(0.4), fontSize: 16),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
