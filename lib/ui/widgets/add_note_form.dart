be0111d22b8f52517579c841ea54d98f47975689
Nat3y <khalednatey85@gmail.com>
2024-06-25 14:12:00 +0300

    -Change add note bottom sheet loading.

--- lib/ui/widgets/add_note_form.dart
+++ lib/ui/widgets/add_note_form.dart
@@ -49,20 +49,25 @@ class _AddNoteFormState extends State<AddNoteForm> {
           const SizedBox(
             height: 16,
           ),
-          CustomButton(
-            onTap: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-                var noteModel = NoteModel(title: title!,
-                    subTitle: subTitle!,
-                    date: DateTime.now().toString(),
-                    color: Colors.blue.value);
-                BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
-                Navigator.of(context).pop();
-              } else {
-                autoValidateMode = AutovalidateMode.always;
-                setState(() {});
-              }
+          BlocBuilder<AddNoteCubit, AddNoteState>(
+            builder: (context, state) {
+              return CustomButton(
+                isLoading: state is AddNoteLoading ? true : false,
+                onTap: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                    var noteModel = NoteModel(title: title!,
+                        subTitle: subTitle!,
+                        date: DateTime.now().toString(),
+                        color: Colors.blue.value);
+                    BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
+                    // Navigator.of(context).pop();
+                  } else {
+                    autoValidateMode = AutovalidateMode.always;
+                    setState(() {});
+                  }
+                },
+              );
             },
           ),
           const SizedBox(
--- lib/ui/widgets/custom_button.dart
+++ lib/ui/widgets/custom_button.dart
@@ -3,7 +3,8 @@ import 'package:notes_app/constants/app_colors.dart';
 
 class CustomButton extends StatelessWidget {
   final Function()? onTap;
-  const CustomButton({super.key, this.onTap});
+  final bool isLoading;
+  const CustomButton({super.key, this.onTap, this.isLoading = false});
 
   @override
   Widget build(BuildContext context) {
@@ -16,8 +17,11 @@ class CustomButton extends StatelessWidget {
           color: AppColors.primaryColor,
           borderRadius: BorderRadius.circular(8)
         ),
-        child: const Center(
-          child: Text("Add", style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.w400),),
+        child: Center(
+          child: isLoading ? const SizedBox(
+            width: 24,
+            height: 24,
+              child: CircularProgressIndicator(color: Colors.black,)) : const Text("Add", style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.w400),),
         ),
       ),
     );
