cc79b57512864a0fabddf6262ab66f4c1f25454d
Nat3y <khalednatey85@gmail.com>
2024-06-28 14:53:44 +0300

    -colors list view part2.

--- lib/ui/widgets/colors_list_view.dart
+++ lib/ui/widgets/colors_list_view.dart
@@ -1,28 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
+  final bool isSelected;
+  final Color color;
+  const ColorItem({super.key, required this.isSelected, required this.color});
 
   @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(
+    return isSelected ?  CircleAvatar(
+      backgroundColor: Colors.white,
       radius: 38,
-      backgroundColor: Colors.blue,
+      child: CircleAvatar(
+        radius: 34,
+        backgroundColor: color,
+      ),
+    ) : CircleAvatar(
+      radius: 38,
+      backgroundColor: color,
     );
   }
 }
 
-class ColorsListView extends StatelessWidget {
-  const ColorsListView({super.key});
+class ColorsListView extends StatefulWidget {
+  const ColorsListView({super.key,});
 
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  List<Color> colors = [
+    Color(0xffAc3931),
+    Color(0xffE5D352),
+    Color(0xffD9E76c),
+    Color(0xff537D8D),
+    Color(0xff482C3D),
+  ];
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 38 * 2,
       child: ListView.builder(
           scrollDirection: Axis.horizontal,
-          itemCount: 10,
-          itemBuilder: (context, index) => const ColorItem()),
+          itemCount: colors.length,
+          itemBuilder: (context, index) => Padding(
+            padding: EdgeInsets.symmetric(horizontal: 6),
+            child: GestureDetector(
+              onTap: () {
+                currentIndex = index;
+                setState(() {});
+              },
+              child: ColorItem(
+                isSelected: currentIndex == index ? true : false ,
+                color: colors[index],
+              ),
+            ),
+          )),
     );
   }
 }
